2f80d120be2913f02d97f9473c57b5e52ae01935
VadimTrubay <vadnetvadnet@ukr.net>
2022-11-05 14:29:00 +0200

    main

--- randome_number_6_52.py
+++ randome_number_6_52.py
@@ -1,17 +1,19 @@
 import random
 import time
 
-print("Это 6 случайных цифр из диапазона  от 1 до 52: ")
-print('======================')
+print("Это 6 случайных шаров из барабана  от 1 до 52: ")
+print('==============================================')
 start_list = list(range(1, 53))
 new_list = []
+time.sleep(1)
 for i in range(1, 7):
     num = random.choice(start_list)
     new_list.append(num)
-    print(num)
+    print(f'Шар номер {i}>: {num}')
     time.sleep(1)
     start_list.remove(num)
 time.sleep(1)
+print('======================')
 print('в порядке выпадения')
 print(', '.join(map(str, new_list)))
 time.sleep(2)
@@ -20,3 +22,4 @@ print('после сортировки по возрастанию')
 # print(str(new_list).strip('[]'))
 list_sort = sorted(new_list)
 print(*list_sort, sep=', ')
+input()
