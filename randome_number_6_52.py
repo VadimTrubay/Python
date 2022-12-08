e175fc5271b943e1c48b4508636421f5fa022682
VadimTrubay <vadnetvadnet@ukr.net>
2022-12-08 13:30:15 +0200

    main

--- randome_number_6_52.py
+++ randome_number_6_52.py
@@ -6,6 +6,7 @@ print('Это 6 случайных шаров из барабана  от 1 до
 start_list = list(range(1, 53))
 new_list = []
 time.sleep(1)
+
 for i in range(1, 7):
     num = random.choice(start_list)
     new_list.append(num)
@@ -13,6 +14,7 @@ for i in range(1, 7):
     time.sleep(1)
     start_list.remove(num)
 time.sleep(2)
+
 print('======================\n' + \
       'в порядке выпадения')
 print(', '.join(map(str, new_list)))
